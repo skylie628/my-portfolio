b31840e976b4b20a0eb9ae08cf9ea24d18c52ef8
skylie628 <khuong.dinhbkk17@hcmut.edu.vn>
2024-04-20 18:03:09 +0700

    fix: children type

--- src/components/mdx-components/MCodeBlock/index.tsx
+++ src/components/mdx-components/MCodeBlock/index.tsx
@@ -4,15 +4,17 @@ import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 interface MCodeBlockProps {
   children?: ReactNode;
 }
-const MCodeBlock = (props: MCodeBlockProps) => {
+const MCodeBlock = ({ children, ...props }: MCodeBlockProps) => {
   return (
     <SyntaxHighlighter
       showLineNumbers
       language="javascript"
-      style={{ ...dracula, backgroundColor: "#111!important" }}
+      style={dracula}
       {...props}
       className="font-normal rounded-md p-5 my-5 overflow-x-auto bg-nones !bg-[#111]"
-    />
+    >
+      {children as string | string[]}
+    </SyntaxHighlighter>
   );
 };
 
