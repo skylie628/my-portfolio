991e1758bf8b611d3d79cee1d7e67237be055101
skylie628 <khuong.dinhbkk17@hcmut.edu.vn>
2024-04-20 16:17:40 +0700

    feat: setting up ci/cd workflow + docker

--- src/app/(child)/blogs/[slug]/page.tsx
+++ src/app/(child)/blogs/[slug]/page.tsx
@@ -7,6 +7,10 @@ import { getPostData } from "@/lib/actions/blog";
 //types
 import type { Metadata, ResolvingMetadata } from "next";
 
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 export async function generateMetadata(
   { params, searchParams }: Props,
   parent: ResolvingMetadata
